018357114aeeda58fc0ba48a405e8a8920f8d724
SeongJae Park <sj38.park@gmail.com>
2016-09-23 16:46:45 +0900

    c_type_sz: Use macro for type size print
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- c_type_sz.c
+++ c_type_sz.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define pr_type_sz(type)	\
+	printf(#type ": %zu bytes\n", sizeof(type));
+
 int main(void)
 {
-	printf("size of char: %zu\n", sizeof(char));
-	printf("size of short: %zu\n", sizeof(short));
-	printf("size of int: %zu\n", sizeof(int));
-	printf("size of long: %zu\n", sizeof(long));
-	printf("size of long long: %zu\n", sizeof(long long));
-	printf("size of float: %zu\n", sizeof(float));
-	printf("size of double: %zu\n", sizeof(double));
-	printf("size of uint64_t: %zu\n", sizeof(uint64_t));
-	printf("size of void *: %zu\n", sizeof(void *));
+	pr_type_sz(char);
+	pr_type_sz(short);
+	pr_type_sz(int);
+	pr_type_sz(long);
+	pr_type_sz(long long);
+	pr_type_sz(float);
+	pr_type_sz(double);
+	pr_type_sz(uint64_t);
+	pr_type_sz(void *);
 
 	return 0;
 }
